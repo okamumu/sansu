926f91a1a6b53ead36dbdb0e485049192e6e5e21
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2024-02-12 19:26:38 +0900

    commit

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-import warisan
+import sansuu
 
-@app.route('/')
-def hello_world():
-    problems = warisan.gen_warisan()
-    return render_template('index.html', problems = problems)
+@app.route('/tashisan1')
+def show_tashisan1():
+    num = request.args.get('num', default=10, type=int)
+    problems = sansuu.tashisan1(num)
+    return render_template('keisan1.html', problems=problems)
+
+@app.route('/hikisan1')
+def show_hikisan1():
+    num = request.args.get('num', default=10, type=int)
+    problems = sansuu.hikisan1(num)
+    return render_template('keisan1.html', problems=problems)
+
+@app.route('/kakesan1')
+def show_kakesan1():
+    num = request.args.get('num', default=10, type=int)
+    problems = sansuu.kakesan1(num)
+    return render_template('keisan1.html', problems=problems)
+
+@app.route('/warisan1')
+def show_warisan1():
+    num = request.args.get('num', default=10, type=int)
+    problems = sansuu.warisan1(num)
+    return render_template('keisan1.html', problems=problems)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ sansuu.py
@@ -0,0 +1,41 @@
+import random
+
+def tashisan1(m):
+    questions_set = set()
+    while len(questions_set) < m:
+        num1 = random.randint(0, 9)
+        num2 = random.randint(0, 9)
+        questions_set.add((num1, num2))
+    questions_list = [f"{q[0]} + {q[1]} = ?" for q in questions_set]
+    return questions_list
+
+def hikisan1(m):
+    questions_set = set()
+    while len(questions_set) < m:
+        num1 = random.randint(0, 9)
+        num2 = random.randint(0, 9)
+        (num1, num2) = (num1, num2) if num1 <= num2 else (num2, num1)
+        questions_set.add((num1 + num2, num2))
+    questions_list = [f"{q[0]} - {q[1]} = ?" for q in questions_set]
+    return questions_list
+
+def kakesan1(m):
+    questions_set = set()
+    while len(questions_set) < m:
+        num1 = random.randint(1, 9)
+        num2 = random.randint(1, 9)
+        question = (num1, num2)
+        questions_set.add(question)
+    questions_list = [f"{q[0]} × {q[1]} = ?" for q in questions_set]
+    return questions_list
+
+def warisan1(m):
+    division_questions_set = set()
+    while len(division_questions_set) < m:
+        num1 = random.randint(1, 9)  # Dividend
+        num2 = random.randint(1, 9)  # Divisor
+        if (num1 * num2) <= 81 and num1 != 1:
+            question = (num1 * num2, num1)
+            division_questions_set.add(question)
+    division_questions_list = [f"{q[0]} ÷ {q[1]} = ?" for q in division_questions_set]
+    return division_questions_list
